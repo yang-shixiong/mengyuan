e57f20a4b0288b1fc8688718b144dac543b7c039
yangshixiong <164399876@qq.com>
2020-01-03 17:05:54 +0800

    :add:年会抽奖小文件

---
+++ src/page/contancts.js
@@ -0,0 +1,43 @@
+const PersonList = [
+  {
+    id:1,
+    name:"小明",
+  },
+  {
+    id:2,
+    name:"小明2",
+  },
+  {
+    id:3,
+    name:"小明3",
+  },
+  {
+    id:4,
+    name:"小明4",
+  },
+  {
+    id:5,
+    name:"小明5",
+  },{
+    id:6,
+    name:"小明6",
+  },{
+    id:7,
+    name:"小明7",
+  },{
+    id:8,
+    name:"小明8",
+  },{
+    id:9,
+    name:"小明9",
+  },{
+    id:10,
+    name:"小明10",
+  },{
+    id:11,
+    name:"小明11",
+  }
+
+]
+
+export default PersonList
